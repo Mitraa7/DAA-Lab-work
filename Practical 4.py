6773c9bfc6563fdaea551a2d33d4f5b1a92d56ff
Mitraa7 <davemitraa@gmail.com>
2026-08-05 20:03:34 +0530

    Practical 4

---
+++ Practical 4.py
@@ -0,0 +1,57 @@
+# Implementation of Factorial using Iterative and Recursive methods
+
+def iterative_factorial(n):
+    fact = 1
+
+    for i in range(1, n + 1):
+        fact *= i
+
+    return fact
+
+
+def recursive_factorial(n):
+    if n == 0 or n == 1:
+        return 1
+
+    return n * recursive_factorial(n - 1)
+
+
+
+def main():
+    n = int(input("Enter a number: "))
+
+    print("\nFactorial Methods")
+    print("1. Iterative Method")
+    print("2. Recursive Method")
+
+    choice = int(input("\nEnter your choice: "))
+
+    if choice == 1:
+        ans = iterative_factorial(n)
+        print(f"\nFactorial = {ans}")
+
+    elif choice == 2:
+        ans = recursive_factorial(n)
+        print(f"\nFactorial = {ans}")
+
+    else:
+        print("Invalid Choice")
+
+# Iterative Factorial
+# Time Complexity:
+# Best Case    : O(n)
+# Average Case : O(n)
+# Worst Case   : O(n)
+#
+# Space Complexity:
+# O(1)
+
+# Recursive Factorial
+#
+# Time Complexity:
+# Best Case    : O(n)
+# Average Case : O(n)
+# Worst Case   : O(n)
+#
+# Space Complexity:
+# O(n)
